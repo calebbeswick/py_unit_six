6dc5ff7758516793f728aba14b958b662b253a74
calebbeswick <beswica@ssfs.org>
2021-11-03 14:28:01 -0400

    unit 6 project

--- assignment_six.py
+++ assignment_six.py
@@ -1,21 +1,22 @@
 #
 #
 #
-
 mytext = open("unit6test", "r")
 listOfFileContents = mytext.readlines()
+
+
 def file_calculations(listOfFileContents):
     lines = 0
-    words = ""
+    z = 0
+    words = 0
     characters = 0
     lines = len(listOfFileContents)
     for x in range(lines):
         list1 = listOfFileContents[x]
-        words = list1.split(" ")
-        x = x +1
+        s = list1.split(" ")
+        z = z + len(s)
         characters = characters + len(list1)
     print(characters)
     print(lines)
     print(words)
-
 file_calculations(listOfFileContents)
