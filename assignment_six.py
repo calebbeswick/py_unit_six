d1cf2bec00b5cf3a16e32964569222dc83acdee6
calebbeswick <beswica@ssfs.org>
2021-11-04 14:45:49 -0400

    Final commit of unit 6 project

--- assignment_six.py
+++ assignment_six.py
@@ -1,22 +1,20 @@
-#
-#
-#
 mytext = open("unit6test", "r")
 listOfFileContents = mytext.readlines()
 
 
 def file_calculations(listOfFileContents):
     lines = 0
-    z = 0
     words = 0
     characters = 0
     lines = len(listOfFileContents)
     for x in range(lines):
         list1 = listOfFileContents[x]
         s = list1.split(" ")
-        z = z + len(s)
+        words = words + len(s)
         characters = characters + len(list1)
-    print(characters)
-    print(lines)
-    print(words)
-file_calculations(listOfFileContents)
+    print("There are ", lines, "lines, ", words, "words and", characters, "characters.")
+
+def main():
+    file_calculations(listOfFileContents)
+
+main()
